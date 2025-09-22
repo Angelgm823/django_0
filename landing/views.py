49a29e709e5b5cd1aee737b73e2050b0a265ebb5
angelgm <gomezmendezangel100@gmail.com>
2025-09-22 13:00:00 -0600

    tags for y url

--- landing/views.py
+++ landing/views.py
@@ -3,11 +3,23 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def home(request):
+    stack = [
+        {'id': 'python', 'name': 'python3'},
+        {'id': 'Angular', 'name': 'Angular v20'},
+        {'id': 'c++', 'name': 'c++'},
+        {'id': 'javascript', 'name': 'js'},
+    ]
     return render(request, "landing/landing.html", {
         'nombre': 'Angel',
         'edad': 25,
         'fecha': datetime.today(),
+        'stack': stack
     })
+
+
+def stack_detail(request, tool):
+    return HttpResponse(f'tecnologia: {tool}')
