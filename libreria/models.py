60a5fcfbe5659fb7261c197496357a06ae7c274e
AngelGM <gomezmendezangel100@gmail.com>
2025-10-02 11:45:55 -0600

    base de datos ORM Django

---
+++ libreria/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    
+    
+class Libros(models.Model):
+    titulo = models.CharField(max_length=250)
+    fecha_publicacion = models.DateField(null=True, blank=True)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+    paginas = models.IntegerField()
+    isbn = models.CharField(max_length=60)
