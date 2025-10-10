e11d48ad31fb7389c3f04ddafef6936acd2a3a5c
angel gomez mendez <gomezmendezangel100@gmail.com>
2025-10-10 11:53:44 -0600

    nuevo commit

--- libreria/models.py
+++ libreria/models.py
@@ -2,14 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Autor(models.Model):
     nombre = models.CharField(max_length=100)
     fecha_nacimiento = models.DateField(null=True, blank=True)
     
-    
+    def __str__(self):
+        return self.nombre
+
+
 class Libros(models.Model):
     titulo = models.CharField(max_length=250)
     fecha_publicacion = models.DateField(null=True, blank=True)
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name="libros")
     paginas = models.IntegerField()
     isbn = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.titulo
