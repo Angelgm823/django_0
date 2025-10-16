759c13dd35b3dd62daad04f1c436a1796cff142d
angelgm <gomezmendezangel100@gmail.com>
2025-10-16 12:36:54 -0600

    creación de tablas uno a uno (Detalles) y muchos a muchos (generos)

--- libreria/models.py
+++ libreria/models.py
@@ -10,6 +10,11 @@ class Autor(models.Model):
     def __str__(self):
         return self.nombre
 
+class Genero(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
 
 class Libros(models.Model):
     titulo = models.CharField(max_length=250)
@@ -17,6 +22,14 @@ class Libros(models.Model):
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name="libros")
     paginas = models.IntegerField()
     isbn = models.CharField(max_length=60)
+    generos = models.ManyToManyField(Genero, related_name='libros')
 
     def __str__(self):
         return self.titulo
+
+
+class DetallesLibro(models.Model):
+    resumen = models.TextField()
+    portada_url = models.CharField()
+    lenguaje = models.CharField()
+    libro = models.OneToOneField(Libros, on_delete=models.CASCADE, related_name='detalle')
