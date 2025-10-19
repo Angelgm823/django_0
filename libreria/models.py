cecdf4dad72d2436111aabcada076504a507a811
angel gomez mendez <gomezmendezangel100@gmail.com>
2025-10-18 23:34:37 -0600

    modelo de usuario, critica, pestamo

--- libreria/models.py
+++ libreria/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -33,3 +34,25 @@ class DetallesLibro(models.Model):
     portada_url = models.CharField()
     lenguaje = models.CharField()
     libro = models.OneToOneField(Libros, on_delete=models.CASCADE, related_name='detalle')
+
+
+class Critica(models.Model):
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libros, on_delete=models.CASCADE, related_name='critica')
+    calificacion = models.PositiveBigIntegerField()
+    texto = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.usuario} -> {self.libro.titulo} ({self.calificacion})'
+
+
+class Prestamo(models.Model):
+     usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+     libro = models.ForeignKey(Libros, on_delete=models.CASCADE, related_name='prestamos')
+     fecha_prestamo = models.DateTimeField(auto_now_add=True)
+     fecha_ingreso = models.DateTimeField(null=True, blank=True)
+     estado = models.BooleanField(default=False)
+
+     def __str__(self):
+         return f'{self.usuario} -> {self.libro.titulo} ({'Devuelto' if self.estado else 'Prestado'})'
